2b162ddabd1c163677c3c2d909f9d4516ee4d6e0
Phil Gresham <philgresham@gmail.com>
2020-08-22 19:12:33 -0700

    Whatever

--- frontend/components/signature/SignatureForm.jsx
+++ frontend/components/signature/SignatureForm.jsx
@@ -6,11 +6,11 @@ import { SigPropTypeShape } from '../propTypes';
 const SignatureForm = ({ sig }) => {
   const canvas = document.querySelector('sig-canvas');
 
-  // const signaturePad = new SignaturePad(canvas);
+  const signaturePad = new SignaturePad(canvas);
   return (
     <div>
       <h1>Signature Page</h1>
-      {/* <canvas id="sig-canvas" height={200} width={500} /> */}
+      <canvas id="sig-canvas" height={200} width={500} />
       <pre>{JSON.stringify(sig, null, 2)}</pre>
     </div>
   );
