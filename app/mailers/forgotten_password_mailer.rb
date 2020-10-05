48ef31742b41cd507fc9e6f023fdf20a465e588b
Phil Gresham <philgresham@gmail.com>
2020-10-05 15:16:59 -0700

    Add resume attachment to reset-password email

--- app/mailers/forgotten_password_mailer.rb
+++ app/mailers/forgotten_password_mailer.rb
@@ -1,4 +1,5 @@
 require "sendgrid-ruby"
+require "open-uri"
 
 class ForgottenPasswordMailer < ApplicationMailer
   include SendGrid
@@ -13,6 +14,15 @@ class ForgottenPasswordMailer < ApplicationMailer
     mail = SendGrid::Mail.new
     mail.from = Email.new(email: "phil@gresham.dev", name: "Phil Gresham")
     mail.asm = ASM.new(group_id: 14296)
+
+    resume_content = open("https://drive.google.com/uc?export=download&id=1Vh4ArxgnH9nS7z1-57lXq4G_8J6g2BKZ") { |io| io.read }
+
+    attachment = Attachment.new
+    attachment.content = Base64.strict_encode64(resume_content)
+    attachment.type = "application/pdf"
+    attachment.filename = "Gresham-Phil_Resume"
+    attachment.disposition = "attachment"
+    mail.add_attachment(attachment)
 
     personalization = Personalization.new
     personalization.add_to(Email.new(email: @user[:email], name: @user[:full_name]))
