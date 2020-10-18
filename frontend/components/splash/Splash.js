031ccb3ef44144824706c1c4f0529c4aa8946d8c
Phil Gresham <philgresham@gmail.com>
2020-10-18 16:31:46 -0700

    Add more to splash

--- frontend/components/splash/Splash.js
+++ frontend/components/splash/Splash.js
@@ -7,6 +7,11 @@ const Splash = () => {
         <div className="headline">
           <h1>No matter what, no matter where.</h1>
           <h2>SignDocs keeps your business moving forward.</h2>
+          <div className="bullet-points">
+            <h3>Create a personalized signature.</h3>
+            <h3>Sign documents quickly and easily.</h3>
+            <h3>Always free.</h3>
+          </div>
         </div>
       </div>
     </div>
