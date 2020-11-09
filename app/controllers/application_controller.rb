1c6f03f38c964729c6039c2d63cdb0846b8eeda0
Phil Gresham <philgresham@gmail.com>
2020-11-09 13:11:04 -0800

    Pull out auth logic in ApplicationController to new concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,45 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include AuthConcern
+  
   helper_method :current_user, :logged_in?
-
-  private
-
-  # rescue_from User::NotAuthorized, with: :user_not_authorized
-  # def user_not_authorized
-  # flash[:error] = "You don't have access to this section."
-  # redirect_back(fallback_location: root_path)
-  # render json: { user: ["You are not authorized to do that."] }
-  # https://edgeguides.rubyonrails.org/action_controller_overview.html#the-default-500-and-404-templates
-  # end
-
-  def current_user
-    return nil unless session[:session_token]
-    @current_user ||= User.find_by(session_token: session[:session_token])
-  end
-
-  def login!(user)
-    session[:session_token] = user.reset_session_token!
-    @current_user = user
-  end
-
-  def logout!
-    @current_user.reset_session_token!
-    @current_user = nil
-    session[:session_token] = nil
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
-  def require_logged_in
-    unless current_user
-      render json: { base: ["invalid credentials"] }, status: 401
-    end
-  end
-
-  def require_logged_out
-    # redirect_to users_url if logged_in?
-  end
 end
